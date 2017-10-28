83cbe4a981c5c604c7d0ca71e5f6665982bd6b33
Steven Carpenter <stevegcarpenter@gmail.com>
2017-10-28 07:59:33 -0700

    Added check to see if the external monitor was connected and launch the appropriate xrandr command.

--- display-hidpi/.screenlayout/layout.sh
+++ display-hidpi/.screenlayout/layout.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+if xrandr | grep -q 'HDMI1 connected'; then
+  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
+else
+  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+fi
