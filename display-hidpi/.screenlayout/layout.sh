480d3e1ec403c58b74d90fded377241e3163cdc6
Steven Carpenter <stevegcarpenter@gmail.com>
2026-07-08 13:44:51 -0500

    Remove obsolete Linux/WM and editor configs
    Delete unused configs for X11 (Xresources, xinit), i3/polybar/dunst/compton,
    chunkwm/yabai/skhd, termite, mercurial, tmux-mac, and VS Code.

--- display-hidpi/.screenlayout/layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if xrandr | grep -q 'HDMI1 connected'; then
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
-else
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-fi
