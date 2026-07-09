e066f885e1c00f7042ffea1ce1540aed7669db26
Steve Carpenter <stevencarpenter@commonroom.io>
2026-07-09 06:02:08 -0500

    Enable line numbers in nvim

--- vim/.config/nvim/lua/core/options.lua
+++ vim/.config/nvim/lua/core/options.lua
@@ -1,8 +1,8 @@
 local opt = vim.opt -- for conciseness
 
 -- line numbers
---opt.relativenumber = true -- show relative line numbers
---opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 --opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
